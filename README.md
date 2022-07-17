67172be9f91a4e568148fe84ac6d1e6638dbb79a
okivictor <102834999+okivictor@users.noreply.github.com>
2022-07-17 18:13:04 +0200

    update code using "pop"

--- README.md
+++ README.md
@@ -4,3 +4,4 @@ edit JS code for assignment (arrays)
 implement various applications of "for loop" 
 add JS objects
 update code
+update code to delete item from array
--- hw.js
+++ hw.js
@@ -1,7 +1,9 @@
 /* EXERCISE 1
   Given the object below, write a piece of code for programmatically removing the last skill from the skills array inside the me object.
  */
+var items = ["victor", "Nigerian", "calabar"]
 
+console.log(items.pop);
 /* EXERCISE 2
 Write a piece of code to create an array of only ODD numbers from 1 to 100*/
 
